b7a1890f15c09e7ead7233827f3ee19b52441c1a
Marshal10 <marshalmaleane123@gmail.com>
2023-12-30 21:53:50 +0530

    Save password functinality

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save_pwd():
+    website=web_input.get()
+    email=email_input.get()
+    password=pwd_input.get()
+    with open("data.txt",mode="a+") as file:
+        file.write(f"{website} | {email} | {password}\n")
+    web_input.delete(0,'end')
+    pwd_input.delete(0,'end')
+    web_input.focus()
 # ---------------------------- UI SETUP ------------------------------- #
 window=Tk()
 window.title("Password Manager")
@@ -17,12 +25,15 @@ web_lbl=Label(text="Website:")
 web_lbl.grid(column=0,row=1,pady=5)
 
 web_input=Entry()
+web_input.focus()
 web_input.grid(column=1,row=1,pady=5,columnspan=2,sticky=W+E,padx=10)
+
 
 email_lbl=Label(text="Email/Username:")
 email_lbl.grid(column=0,row=2,pady=5)
 
 email_input=Entry()
+email_input.insert(END,'xyz@gmail.com')
 email_input.grid(column=1,row=2,pady=5,columnspan=2,sticky=W+E,padx=10)
 
 pwd_lbl=Label(text="Password:")
@@ -34,7 +45,7 @@ pwd_input.grid(column=1,row=3,padx=10,pady=5,sticky=W+E)
 generate_btn=Button(text="Generate Password")
 generate_btn.grid(column=2,row=3,padx=10,pady=5)
 
-add_btn=Button(text="Add",width=36)
+add_btn=Button(text="Add",width=36,command=save_pwd)
 add_btn.grid(column=1,row=4,padx=10,columnspan=2,pady=5,sticky=W+E)
 
 window.mainloop()
