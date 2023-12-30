6e4ec2059d447c2f9da6e9e1b7b13f3e2b839743
Marshal10 <marshalmaleane123@gmail.com>
2023-12-31 00:32:29 +0530

    Save in json format

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox 
 from password import Password
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pwd():
@@ -18,16 +19,31 @@ def save_pwd():
     website=web_input.get().title()
     email=email_input.get()
     password=pwd_input.get()
-    if len(website)<=0 or len(password)<=0:
+    new_data={
+                website:{
+                    "email":email,
+                     "password":password
+                }
+            }
+    if len(website)==0 or len(password)==0 or len(email)==0:
         messagebox.showerror("Oops","Please don't leave any of the fields empty")
         return
     confirm_save=messagebox.askokcancel(title=website,message=f"Email: {email}\nPassword: {password}\n is this ok?")
     if confirm_save:
-        with open("data.txt",mode="a+") as file:
-            file.write(f"{website} | {email} | {password}\n")
-        web_input.delete(0,'end')
-        pwd_input.delete(0,'end')
-        web_input.focus()
+        try:
+            with open("data.json",mode="r") as file:
+                data=json.load(file)
+        except FileNotFoundError:
+            with open("data.json",mode="w") as file:
+                json.dump(new_data,file,indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json",mode="w") as file:
+                json.dump(data,file,indent=4)
+        finally:
+            web_input.delete(0,'end')
+            pwd_input.delete(0,'end')
+            web_input.focus()
         
 # ---------------------------- UI SETUP ------------------------------- #
 window=Tk()
@@ -42,14 +58,14 @@ canvas.grid(column=1,row=0)
 web_lbl=Label(text="Website:")
 web_lbl.grid(column=0,row=1,pady=5)
 
-web_input=Entry()
+web_input=Entry(width=21)
 web_input.focus()
-web_input.grid(column=1,row=1,pady=5,columnspan=2,sticky=W+E,padx=10)
+web_input.grid(column=1,row=1,pady=5,sticky=W+E,padx=10)
 
 email_lbl=Label(text="Email/Username:")
 email_lbl.grid(column=0,row=2,pady=5)
 
-email_input=Entry()
+email_input=Entry(width=35)
 email_input.insert(END,'xyz@gmail.com')
 email_input.grid(column=1,row=2,pady=5,columnspan=2,sticky=W+E,padx=10)
 
@@ -64,5 +80,8 @@ generate_btn.grid(column=2,row=3,padx=10,pady=5)
 
 add_btn=Button(text="Add",width=36,command=save_pwd)
 add_btn.grid(column=1,row=4,padx=10,columnspan=2,pady=5,sticky=W+E)
+
+search_btn=Button(text="Search")
+search_btn.grid(column=2,row=1,sticky=W+E,padx=10,pady=5)
 
 window.mainloop()
